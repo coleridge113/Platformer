00071f9a8ca60b28b6df990aafc6a8524efc1a73
Jose Luna <josemanuelmluna@gmail.com>
2026-06-18 16:39:26 +0800

    feat: added walls on horizontal window boundaries

--- include/game.h
+++ include/game.h
@@ -9,6 +9,9 @@ struct Game
 {
 
     Block block { windowWidth / 2.f, floorY - 100, 30, 30, GRAY };
+
+    Block wallLeft { 0, 0, 1, windowHeight, BLACK };
+    Block wallRight { windowWidth, 0, 1, windowHeight, BLACK };
     Block floor { 0, floorY, windowWidth, 1, DARKBLUE };
 
     void draw()
@@ -16,32 +19,25 @@ struct Game
         handleInput();
 
         block.update();
-        handleCollision(block, floor);
+        handleCollisionY(block, floor);
+        handleCollisionLeft(block, wallLeft);
+        handleCollisionRight(block, wallRight);
 
         block.draw();
         floor.draw();
+        wallLeft.draw();
+        wallRight.draw();
 
     }
 
     void handleInput()
     {
-        if (IsKeyPressed(KEY_SPACE))
-        {
-            block.jump();
-        }
-
-        if (IsKeyDown(KEY_J))
-        {
-            block.move(-1, 0);
-        }
-
-        if (IsKeyDown(KEY_L))
-        {
-            block.move(1, 0);
-        }
+        if (IsKeyPressed(KEY_SPACE))    block.jump();
+        if (IsKeyDown(KEY_J))           block.move(-1, 0);
+        if (IsKeyDown(KEY_L))           block.move(1, 0);
     }
 
-    void handleCollision(Block& a, Block& b)
+    void handleCollisionY(Block& a, Block& b)
     {
         float bottomA = a.posY + a.height;
         if (bottomA >= b.posY)
@@ -52,8 +48,23 @@ struct Game
         }
     }
 
-    void handleMovement(int keyPressed)
+    void handleCollisionLeft(Block& a, Block& b)
     {
-        
+        float left = a.posX;
+
+        if (left <= b.posX)
+        {
+            a.posX = b.posX;
+        }
+    }
+
+    void handleCollisionRight(Block& a, Block& b)
+    {
+        float rightA = a.posX + a.width;
+
+        if (rightA >= b.posX)
+        {
+            a.posX = b.posX -  a.width;
+        }
     }
 };
